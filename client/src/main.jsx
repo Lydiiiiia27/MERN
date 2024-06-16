2e765eff6534f6d1fe587e4221eb1f7a943e9d4a
tjinjing <tjinjing@unc.edu>
2024-06-16 14:52:27 -0400

    finish section 2 set up steps

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // StrictMode is a tool for highlighting potential problems in an application. will have more logs in the console!
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
